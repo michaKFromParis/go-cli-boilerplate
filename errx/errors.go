26a2e02df48c2f9f2ae59278e49511076277169d
Michel Courtine <github@michak.net>
2019-01-30 15:43:07 +0100

    added errx
    Signed-off-by: Michel Courtine <github@michak.net>

---
+++ errx/errors.go
@@ -0,0 +1,22 @@
+// FROM https://github.com/henrmota/errors-handling-example
+
+package errx
+
+import (
+	log "github.com/Sirupsen/logrus"
+	"github.com/joomcode/errorx"
+)
+
+// Fatal is used terminate the program with an error
+func Fatal(err error) {
+	log.Fatalf("%v", err)
+}
+
+// Fatalf is used terminate the program with an error and decorate the last error with a message
+func Fatalf(err error, message string) {
+	if err != nil {
+		Fatal(errorx.Decorate(err, message))
+	} else {
+		log.Fatalf(message)
+	}
+}
